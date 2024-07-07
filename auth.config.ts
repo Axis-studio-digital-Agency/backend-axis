765bfcdeb98e04e6998e9ff34f909fc2920d548d
Killian <killian.boularand@outlook.fr>
2024-07-07 17:42:42 +0200

    v1.1

---
+++ auth.config.ts
@@ -0,0 +1,20 @@
+import GitHub from "next-auth/providers/github";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  /* Types de conection: github, google ? */
+  providers: [
+    /* connection via github */
+    GitHub({
+      profile(profile) {
+        return {
+          id: profile.id.toString(), // Convertir l'ID en chaîne de caractères
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+          role: profile.role ?? "user"
+        };
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
---
+++ auth.ts
@@ -0,0 +1,28 @@
+// auth.ts
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import authConfig from "./auth.config";
+
+const prisma = new PrismaClient(); // generation du client prisma
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  session: {
+    strategy: "jwt", 
+  },
+  adapter: PrismaAdapter(prisma), 
+  ...authConfig, 
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log(token);
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user = token as any;
+      }
+      return session;
+    },
+  },
+
+});
